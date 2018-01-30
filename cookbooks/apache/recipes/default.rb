3bf9e8719084daa39e9a4dea8979949a5dca6928
alkesh.d <alkeshd@mailinator.com>
2018-01-30 17:52:35 +0000

    uploading new recipe with node attribute feature

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,12 +5,18 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 # Install apache
 
+if node['package_family'] == 'rhel'
+ package='httpd'
+elsif node['package_family'] == 'debian'
+ package='apache2'
+end
+
 package 'apache2' do
- package_name 'httpd'
+ package_name package
  action :install
 end
 
 service 'start_apache' do
- service_name 'httpd'
+ service_name package
  action [:enable,:start]
 end
